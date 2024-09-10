15a683afc0c3e730e286d1da1784380d716815cc
trung2891 <trungnv280201@gmail.com>
2024-09-10 11:41:02 +0700

    chore: test zapin with diff token

--- contracts/zapper/src/tests/zapin.rs
+++ contracts/zapper/src/tests/zapin.rs
@@ -75,7 +75,6 @@ fn init_basic_v3_pool(
     let liquidity_delta = Liquidity::new(2u128.pow(63) - 1);
 
     let lower_tick_index = -20;
-    let middle_tick_index = -10;
     let upper_tick_index = 10;
 
     create_position!(
@@ -95,8 +94,8 @@ fn init_basic_v3_pool(
         app,
         config.dex_v3,
         pool_key_2,
-        lower_tick_index - 20,
-        middle_tick_index,
+        lower_tick_index,
+        upper_tick_index,
         liquidity_delta,
         SqrtPrice::new(0),
         SqrtPrice::max_instance(),
@@ -126,7 +125,6 @@ fn test_zap_in_with_same_token() {
     let protocol_fee = Percentage::from_scale(6, 3);
     let fee_tier = FeeTier::new(protocol_fee, 1).unwrap();
     let pool_key_x_y = PoolKey::new(token_x.to_string(), token_y.to_string(), fee_tier).unwrap();
-    let pool_key_y_z = PoolKey::new(token_y.to_string(), token_z.to_string(), fee_tier).unwrap();
 
     let tick_lower_index = 0;
     let tick_upper_index = 10;
@@ -228,4 +226,125 @@ fn test_zap_in_with_same_token() {
     // get all positions
     let all_positions = get_all_positions!(app, config.dex_v3, bob);
     assert_eq!(all_positions.len(), 2);
+}
+
+#[test]
+fn test_zap_in_by_diff_token() {
+    let (mut app, accounts) = MockApp::new(&[
+        ("alice", &coins(100_000_000_000, FEE_DENOM)),
+        ("bob", &coins(100_000_000_000, FEE_DENOM)),
+    ]);
+    let alice = &accounts[0];
+    let bob = &accounts[1];
+    let initial_amount = 10u128.pow(20);
+    let (token_x, token_y, token_z) =
+        create_3_tokens!(app, initial_amount, initial_amount, initial_amount, alice);
+
+    let zapper = create_zapper!(app, alice);
+    let config = app.get_zapper_config(zapper.as_str()).unwrap();
+
+    init_basic_v3_pool(
+        &mut app, &zapper, &token_x, &token_y, &token_z, &alice, &bob,
+    );
+
+    let protocol_fee = Percentage::from_scale(6, 3);
+    let fee_tier = FeeTier::new(protocol_fee, 1).unwrap();
+    let pool_key_x_y = PoolKey::new(token_x.to_string(), token_y.to_string(), fee_tier).unwrap();
+    let pool_key_y_z = PoolKey::new(token_y.to_string(), token_z.to_string(), fee_tier).unwrap();
+
+    let tick_lower_index = 0;
+    let tick_upper_index = 10;
+
+    // asset_in = token_z
+    let asset_in = Asset {
+        info: AssetInfo::Token {
+            contract_addr: token_z.clone(),
+        },
+        amount: Uint128::new(1000),
+    };
+
+    // missing route swap to x
+    app.zap_in_liquidity(
+        &bob,
+        zapper.as_str(),
+        pool_key_x_y.clone(),
+        tick_lower_index,
+        tick_upper_index,
+        &asset_in,
+        Uint128::new(500),
+        Uint128::new(500),
+        None,
+        Some(vec![SwapOperation::SwapV3 {
+            pool_key: pool_key_y_z.clone(),
+            x_to_y: false,
+        }]),
+        None,
+        None,
+    )
+    .unwrap_err()
+    .root_cause()
+    .to_string()
+    .contains("Missing route swap");
+
+    // missing route swap to y
+    app.zap_in_liquidity(
+        &bob,
+        zapper.as_str(),
+        pool_key_x_y.clone(),
+        tick_lower_index,
+        tick_upper_index,
+        &asset_in,
+        Uint128::new(500),
+        Uint128::new(500),
+        Some(vec![
+            SwapOperation::SwapV3 {
+                pool_key: pool_key_y_z.clone(),
+                x_to_y: false,
+            },
+            SwapOperation::SwapV3 {
+                pool_key: pool_key_x_y.clone(),
+                x_to_y: false,
+            },
+        ]),
+        None,
+        None,
+        None,
+    )
+    .unwrap_err()
+    .root_cause()
+    .to_string()
+    .contains("Missing route swap");
+
+    // add successful
+    app.zap_in_liquidity(
+        &bob,
+        zapper.as_str(),
+        pool_key_x_y.clone(),
+        tick_lower_index,
+        tick_upper_index,
+        &asset_in,
+        Uint128::new(500),
+        Uint128::new(500),
+        Some(vec![
+            SwapOperation::SwapV3 {
+                pool_key: pool_key_y_z.clone(),
+                x_to_y: false,
+            },
+            SwapOperation::SwapV3 {
+                pool_key: pool_key_x_y.clone(),
+                x_to_y: false,
+            },
+        ]),
+        Some(vec![SwapOperation::SwapV3 {
+            pool_key: pool_key_y_z.clone(),
+            x_to_y: false,
+        }]),
+        None,
+        None,
+    )
+    .unwrap();
+
+    // get all positions
+    let all_positions = get_all_positions!(app, config.dex_v3, bob);
+    assert_eq!(all_positions.len(), 1);
 }
