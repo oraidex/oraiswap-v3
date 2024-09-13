4dc54fd6853da909578d743bc7f7471b0aba456e
trung2891 <trungnv280201@gmail.com>
2024-09-13 10:11:46 +0700

    chore: add test case for zap-in liquidity out range

--- contracts/zapper/src/tests/zapin.rs
+++ contracts/zapper/src/tests/zapin.rs
@@ -2,6 +2,7 @@ use cosmwasm_std::{coins, Addr, Uint128};
 use decimal::*;
 use oraiswap::mixed_router::SwapOperation;
 use oraiswap_v3_common::asset::{Asset, AssetInfo};
+use oraiswap_v3_common::error::ContractError;
 use oraiswap_v3_common::math::liquidity::Liquidity;
 use oraiswap_v3_common::math::percentage::Percentage;
 use oraiswap_v3_common::math::sqrt_price::{calculate_sqrt_price, SqrtPrice};
@@ -76,7 +77,7 @@ fn init_basic_v3_pool(
     let liquidity_delta = Liquidity::new(2u128.pow(63) - 1);
 
     let lower_tick_index = -20;
-    let upper_tick_index = 10;
+    let upper_tick_index = 20;
 
     create_position!(
         app,
@@ -347,4 +348,164 @@ fn test_zap_in_with_asset_in_lt_total_swap() {
         .unwrap_err();
 
     assert!(err.root_cause().to_string().contains("Invalid fund"));
+}
+
+#[test]
+fn test_zap_in_out_of_range() {
+    let (mut app, accounts) = MockApp::new(&[
+        ("alice", &coins(100_000_000_000, FEE_DENOM)),
+        ("bob", &coins(100_000_000_000, FEE_DENOM)),
+    ]);
+    let alice = &accounts[0];
+    let bob = &accounts[1];
+    let initial_amount = 10u128.pow(20);
+    let (token_x, token_y, token_z) =
+        create_3_tokens!(app, initial_amount, initial_amount, initial_amount, alice);
+
+    let zapper = create_zapper!(app, alice);
+    let config = app.get_zapper_config(zapper.as_str()).unwrap();
+
+    init_basic_v3_pool(
+        &mut app, &zapper, &token_x, &token_y, &token_z, &alice, &bob,
+    );
+
+    let protocol_fee = Percentage::from_scale(6, 3);
+    let fee_tier = FeeTier::new(protocol_fee, 1).unwrap();
+    let pool_key_x_y = PoolKey::new(token_x.to_string(), token_y.to_string(), fee_tier).unwrap();
+    let pool_key_y_z = PoolKey::new(token_y.to_string(), token_z.to_string(), fee_tier).unwrap();
+
+    // asset_in = token_z
+    let asset_in = Asset {
+        info: AssetInfo::Token {
+            contract_addr: token_z.clone(),
+        },
+        amount: Uint128::new(1000),
+    };
+
+    // query pool
+    let pool = get_pool!(app, config.dex_v3, token_x, token_y, fee_tier).unwrap();
+
+    // add position < currentTick
+    let tick_lower_index = pool.current_tick_index - 30;
+    let tick_upper_index = pool.current_tick_index - 20;
+
+    // add fail. liquidity = 0
+    let error = app
+        .zap_in_liquidity(
+            &bob,
+            zapper.as_str(),
+            pool_key_x_y.clone(),
+            tick_lower_index,
+            tick_upper_index,
+            &asset_in,
+            vec![Route {
+                token_in: token_z.to_string(),
+                offer_amount: Uint128::new(500),
+                operations: vec![
+                    SwapOperation::SwapV3 {
+                        pool_key: pool_key_y_z.clone(),
+                        x_to_y: false,
+                    },
+                    SwapOperation::SwapV3 {
+                        pool_key: pool_key_x_y.clone(),
+                        x_to_y: false,
+                    },
+                ],
+                minimum_receive: None,
+            }],
+            None,
+        )
+        .unwrap_err();
+    assert!(error
+        .root_cause()
+        .to_string()
+        .contains(&ContractError::InsufficientLiquidity {}.to_string()));
+
+    // add successful
+    app.zap_in_liquidity(
+        &bob,
+        zapper.as_str(),
+        pool_key_x_y.clone(),
+        tick_lower_index,
+        tick_upper_index,
+        &asset_in,
+        vec![Route {
+            token_in: token_z.to_string(),
+            offer_amount: Uint128::new(500),
+            operations: vec![SwapOperation::SwapV3 {
+                pool_key: pool_key_y_z.clone(),
+                x_to_y: false,
+            }],
+            minimum_receive: None,
+        }],
+        None,
+    )
+    .unwrap();
+
+    // get all positions
+    let all_positions = get_all_positions!(app, config.dex_v3, bob);
+
+    assert_eq!(all_positions.len(), 1);
+
+    // add position < currentTick
+    let tick_lower_index = pool.current_tick_index + 20;
+    let tick_upper_index = pool.current_tick_index + 30;
+
+    // add fail. liquidity = 0
+    let error = app
+        .zap_in_liquidity(
+            &bob,
+            zapper.as_str(),
+            pool_key_x_y.clone(),
+            tick_lower_index,
+            tick_upper_index,
+            &asset_in,
+            vec![Route {
+                token_in: token_z.to_string(),
+                offer_amount: Uint128::new(500),
+                operations: vec![SwapOperation::SwapV3 {
+                    pool_key: pool_key_y_z.clone(),
+                    x_to_y: false,
+                }],
+                minimum_receive: None,
+            }],
+            None,
+        )
+        .unwrap_err();
+    assert!(error
+        .root_cause()
+        .to_string()
+        .contains(&ContractError::InsufficientLiquidity {}.to_string()));
+
+    // add successful
+    app.zap_in_liquidity(
+        &bob,
+        zapper.as_str(),
+        pool_key_x_y.clone(),
+        tick_lower_index,
+        tick_upper_index,
+        &asset_in,
+        vec![Route {
+            token_in: token_z.to_string(),
+            offer_amount: Uint128::new(500),
+            operations: vec![
+                SwapOperation::SwapV3 {
+                    pool_key: pool_key_y_z.clone(),
+                    x_to_y: false,
+                },
+                SwapOperation::SwapV3 {
+                    pool_key: pool_key_x_y.clone(),
+                    x_to_y: false,
+                },
+            ],
+            minimum_receive: None,
+        }],
+        None,
+    )
+    .unwrap();
+
+    // get all positions
+    let all_positions = get_all_positions!(app, config.dex_v3, bob);
+
+    assert_eq!(all_positions.len(), 2);
 }
